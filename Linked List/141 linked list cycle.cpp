82de5c31041b3811660c0eb9f3211acae3e2c4d4
cb-rohit.r <rohit.r@chargebee.com>
2020-06-18 08:20:04 +0530

    Create 141 linked list cycle.cpp

---
+++ Linked List/141 linked list cycle.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+bool hasCycle(ListNode *head) {
+    ListNode *fast_ptr = head, *slow_ptr = head;
+    while (slow_ptr && fast_ptr && fast_ptr->next) {
+        slow_ptr = slow_ptr->next;
+        fast_ptr = fast_ptr->next->next;
+        if (slow_ptr == fast_ptr)
+            return true;
+    }
+    return false;
+}
