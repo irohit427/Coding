4b8ccc29f335845dee260449042a94b064083447
Rohit Raj <irohit427@gmail.com>
2020-10-22 23:10:24 +0530

    Create 96 unique binary search trees.cpp

---
+++ Tree/96 unique binary search trees.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// catalan numbers c(2n, n) / (n + 1)
+int numTrees(int n) {
+    long long ans = 1;
+    for (int i = n + 1; i <= 2 * n; i++) {
+        ans = ans * i / (i - n);
+    }
+    return ans / (n + 1);
+}
