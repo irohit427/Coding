f53240a1b312208ad883f1f83ab50adfc09915ab
Rohit Raj <irohit427@gmail.com>
2020-10-27 23:35:32 +0530

    Create 669 trim a binary search tree.cpp

---
+++ Tree/669 trim a binary search tree.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* trimBST(TreeNode* root, int L, int R) {
+    if (root == nullptr) {
+        return nullptr;
+    }
+
+    if (root->val < L)
+        return trimBST(root->right, L, R);
+    
+    if (root->val > R)
+        return trimBST(root->left, L, R);
+    
+    root->left = trimBST(root->left, L, R);
+    root->right = trimBST(root->right, L, R);
+    return root;
+}
